f7c13c3a4ce6cd9c978404cf8f4ea6bf4b03d055
Łukasz Jabłoński <lukasz.j.pl@interia.pl>
2021-04-16 21:51:02 +0200

    Created models for customer, product, profile-user

--- src/customers/admin.py
+++ src/customers/admin.py
@@ -1,3 +1,7 @@
+import django.contrib.admin
 from django.contrib import admin
+from .models import Customer
 
 # Register your models here.
+
+admin.site.register(Customer)
--- src/products/models.py
+++ src/products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=7, decimal_places=2, help_text="In Polish Złoty")
+    image = models.ImageField(upload_to='Products', default='no_picture.png')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name}-{self.created.strftime('%d/%m/%Y')}"
